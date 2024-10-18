fd89a582ec97d88a6582be6c69aa4bc17e2379f7
Sleepe005 <kholopov005@mail.ru>
2024-10-19 00:44:44 +0300

    feat: validation date vals

--- main.c
+++ main.c
@@ -7,7 +7,7 @@
 #include <stdlib.h>
 
 // Функция проверки валидности вводимых даннх
-bool isSample(char strData[12]){
+bool isSample(char strData[11]){
     bool allDigIsDig = true;
     bool hasSep = true; // __.__.____ данные должны поступать в этом формате
     for(int i = 0; i < strlen(strData); ++i){
@@ -22,7 +22,7 @@ bool isSample(char strData[12]){
 }
 
 // Функция распарсирования строки
-void Split(char data[12], int *value){
+void Split(char data[11], int *value){
     char element[5] = "";
     int pos = 0;
     int j = 0;
@@ -40,6 +40,61 @@ void Split(char data[12], int *value){
     value[j] = atoi(element);
 }
 
+// Функция определения високосного года
+bool visokosYear(int year){
+    bool res;
+
+    if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0){
+        res = true;
+    }
+    else{
+        res = false;
+    }
+
+    return res;
+}
+
+// Функция подсчёёта количества дней в месяце
+int valDayInMonth(int year, int month){
+    bool vis = visokosYear(year);
+
+    int res;
+    month%=12;
+
+    if (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12)
+    {
+        res = 31;
+    }
+    else{
+        if (month == 2){
+            if (vis){
+                res = 29;
+            }
+            else{
+                res = 28;
+            }
+        }
+        else{
+            res = 30;
+        }
+    }
+
+    return res;
+}
+
+// Функция проверки валидность даты
+bool isValidDate(char strDate[11]){
+    int dateValues[3] = {0};
+    Split(strDate, dateValues);
+
+    bool goodDate = false;
+    if(dateValues[1] <= 12 && dateValues[0] <= valDayInMonth(dateValues[2], dateValues[1])){
+        goodDate = true;
+    }
+
+    return goodDate;
+}
+
 
 int main(){
     // Установка языка, инициализация окна и цветов
@@ -50,8 +105,6 @@ int main(){
     init_pair(2, COLOR_GREEN, COLOR_MAGENTA);
     bkgd(COLOR_PAIR(2));
     attrset(COLOR_PAIR(1));
-
-
     clear();
 
     printw("Введите вашу дату рождения (ДД:ММ:ГГГГ): ");
@@ -62,10 +115,24 @@ int main(){
     // считываем дату рождения, пока не получим валидные данные
     while(!haveBirthDay){
         scanw("%s\n", &birthDay);
+
+        bool isSamle = true;    
+        bool isValid = true;    
         if(!isSample(birthDay)){
             clear();
             printw("Неверный формат ввод данных :(\nВведите вашу дату рождения снова: ");
-        }else{
+            strcpy(birthDay, "");
+            isSamle = false;
+        }
+
+        if(isSamle && !isValidDate(birthDay)){
+            clear();
+            printw("Неверный формат ввод данных :(\nВведите вашу дату рождения снова: ");
+            strcpy(birthDay, "");
+            isValid = false;
+        }
+        
+        if(isSamle && isValid){
             haveBirthDay = true;
         }
     }
@@ -79,7 +146,7 @@ int main(){
     }
 
     // Высчитываем старший аркан
-    if(numbersSum > 22){numbersSum -= 22;}
+    if(numbersSum > 22){numbersSum -= 22*(numbersSum/22);}
 
     clear();
     printw("Номер вашего старшего аркана - %d\n", numbersSum);
