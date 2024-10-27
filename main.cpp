2f4d4671372b11bde9b0ea60d2844ffc4c84436e
Sleepe005 <kholopov005@mail.ru>
2024-10-27 23:43:31 +0300

    fix:fix print menu func

--- main.cpp
+++ main.cpp
@@ -98,14 +98,14 @@ bool isValidDate(char strDate[11]){
 
 void printMenu(int cursePosition, bool isBirthDay, char birthDay[11]){
     char menu[4][75] = {
-        "1. Ввести дату",
-        "2. Узнать аркан",
-        "3. Вывести лог",
+        "1. Ввести дату   ",
+        "2. Узнать аркан  ",
+        "3. Вывести лог   ",
         "4. Выход (esc x2)"
     };
 
     for(int i = 1; i <= 4; i++){
-        printw("%s", menu[i]);
+        printw("%s", menu[i-1]);
         
         if(isBirthDay && i == 1){
             printw("    %s", birthDay);
@@ -126,58 +126,95 @@ int main(){
     init_pair(2, COLOR_BLACK, COLOR_MAGENTA);
     bkgd(COLOR_PAIR(2));
     attrset(COLOR_PAIR(1));
-    clear();
 
-    printw("Введите вашу дату рождения (ДД:ММ:ГГГГ): ");
-
+    bool isExit = false;
+    int cursePosition = 1;
+    bool isBirthDay = false;
     char birthDay[11];
-    bool haveBirthDay = false;
 
-    // считываем дату рождения, пока не получим валидные данные
-    while(!haveBirthDay){
-        scanw("%s\n", birthDay);
+    while(!isExit){
+        int key;
+        // clear();
+        printMenu(cursePosition, isBirthDay, birthDay);
+        key = getch();
+        if(key == 27){
+            key = getch();
+            if(key == 91){
+                key = getch();
+                if(key == 65){
+                    cursePosition--;
+                    if(cursePosition < 1){
+                        cursePosition = 4;
+                    }
+                }
+                if(key == 66){
+                    cursePosition++;
+                    if(cursePosition > 4){
+                        cursePosition = 1;
+                    }
+                }
+            }
+            if (key == 27){
+                isExit = true;
+            }
 
-        bool isSamle = true;    
-        bool isValid = true;    
-        if(!(isSample(birthDay) && isValidDate(birthDay))){
-            clear();
-            printw("Неверный формат ввод данных :(\nВведите вашу дату рождения снова: ");
-            strcpy(birthDay, "");
-            isSamle = false;
-        }else{haveBirthDay = true;}
-    }
-
-    std::ofstream datesLog("log.txt", std::ios_base::out || std::ios_base::ate);
-    datesLog << birthDay << '\n';
-    datesLog.close();
-
-    // Суммируем все цифры полученной даты
-    int numbersSum = 0;
-    for(int i = 0; i != strlen(birthDay); ++i){
-        if(isdigit(birthDay[i])){
-            numbersSum += birthDay[i]-'0';
         }
+        clear();
+        
     }
 
-    // Высчитываем старший аркан
-    if(numbersSum > 22){numbersSum -= 22*(numbersSum/22);}
+    // clear();
 
-    clear();
-    printw("Номер вашего старшего аркана - %d\n", numbersSum);
+    // printw("Введите вашу дату рождения (ДД:ММ:ГГГГ): ");
 
-    char line[500];
-    std::ifstream arkan_text("info.txt");
+    // char birthDay[11];
+    // bool haveBirthDay = false;
 
-    for(int i = 0; i < numbersSum - 1; i++){
-        arkan_text.getline(line, 500);
-        // strcpy(line, "");
-    }
-    arkan_text.getline(line, 500);
-    arkan_text.close();
+    // // считываем дату рождения, пока не получим валидные данные
+    // while(!haveBirthDay){
+    //     scanw("%s\n", birthDay);
 
-    printw("Описание вашего аркана: %s\n", line);
+    //     bool isSamle = true;    
+    //     bool isValid = true;    
+    //     if(!(isSample(birthDay) && isValidDate(birthDay))){
+    //         clear();
+    //         printw("Неверный формат ввод данных :(\nВведите вашу дату рождения снова: ");
+    //         strcpy(birthDay, "");
+    //         isSamle = false;
+    //     }else{haveBirthDay = true;}
+    // }
 
-    getch();
+    // std::ofstream datesLog("log.txt", std::ios_base::out || std::ios_base::ate);
+    // datesLog << birthDay << '\n';
+    // datesLog.close();
+
+    // // Суммируем все цифры полученной даты
+    // int numbersSum = 0;
+    // for(int i = 0; i != strlen(birthDay); ++i){
+    //     if(isdigit(birthDay[i])){
+    //         numbersSum += birthDay[i]-'0';
+    //     }
+    // }
+
+    // // Высчитываем старший аркан
+    // if(numbersSum > 22){numbersSum -= 22*(numbersSum/22);}
+
+    // clear();
+    // printw("Номер вашего старшего аркана - %d\n", numbersSum);
+
+    // char line[500];
+    // std::ifstream arkan_text("info.txt");
+
+    // for(int i = 0; i < numbersSum - 1; i++){
+    //     arkan_text.getline(line, 500);
+    //     // strcpy(line, "");
+    // }
+    // arkan_text.getline(line, 500);
+    // arkan_text.close();
+
+    // printw("Описание вашего аркана: %s\n", line);
+
+    // getch();
 
     endwin();
     return 0;
