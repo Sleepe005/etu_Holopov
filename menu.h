4cf80910f765b27759f5e169f60ee7f75df880a9
Sleepe005 <kholopov005@mail.ru>
2024-11-17 18:08:43 +0300

    feat:add check in libs

--- menu.h
+++ menu.h
@@ -1,3 +1,8 @@
+#ifndef MENU_H
+#define MENU_H
+
 void printMenu(int, bool, char[]);
 void doSomething(int, char*, bool*);
 void menuFunc();
+
+#endif MENU_H
--- workWithData.h
+++ workWithData.h
@@ -1,2 +1,7 @@
+#ifndef WORK_WITH_DATA_H
+#define WORK_WITH_DATA_H
+
 bool isSample(char[]);
 void Split(char[], int*);
+
+#endif WORK_WITH_DATA_H
